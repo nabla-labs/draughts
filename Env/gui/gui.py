962d85b9a6d8278087451385175fbdf8251f8989
michaelGentner <38573286+michaelGentner@users.noreply.github.com>
2019-03-18 17:12:05 +0100

    Added settostate function to gui

--- Env/gui/gui.py
+++ Env/gui/gui.py
@@ -1,5 +1,6 @@
 from tkinter import *
-import time
+import numpy as np
+
 class draughtsboard:
 
     def __init__(self):
@@ -9,6 +10,7 @@ class draughtsboard:
         self.boardframeheight = 400
         self.colordark = "saddle brown"
         self.colorwhite = "burlywood3"
+        self.pieces = []
         self.movedisplayframeheight = self.boardframeheight/10
         self.movedisplayframe = Frame(self.window, width=self.boardframewidth, height=self.movedisplayframeheight, bd=2, background="white")
         self.movedisplayframe.pack()
@@ -21,10 +23,9 @@ class draughtsboard:
         
         self.initboard()
         self.initmovedisplay()
-        piece = draughtpiece(self.boardcanvas, "black", int(self.boardframeheight/8), 0)
+                
         
-        piece.place(self.boardposcoordinates[7][7])
-        self.window.mainloop()
+        
 
     def initboard(self):
 
@@ -65,12 +66,38 @@ class draughtsboard:
             self.movedisplaycanvas.itemconfig(self.movedisplay_text, text="It is white's move", fill="black")
         self.window.update()
 
+    def settostate(self, state):
+
+        for piece in self.pieces:
+
+            self.boardcanvas.delete(piece.piece)
+            self.boardcanvas.delete(piece.text)
+
+        for y, row in enumerate(state):
+            for x, field in enumerate(row):
+
+                if field[1] != 0:
+                    
+                    if field[1] == 1 or field[1] == 3:
+                        color = "white"
+                    else:
+                        color = "black"
+
+                    piece = draughtpiece(self.boardcanvas, color, int(self.boardframeheight/8), int(field[2]))
+                    piece.place(self.boardposcoordinates[x][y])
+                    self.pieces.append(piece)
+        self.window.update()
+
 class draughtpiece:
 
     def __init__(self, canvas, color, dim, ID):
 
         self.canvas = canvas
         self.color = color
+        if color == "black":
+            self.textcolor = "white"
+        else:
+            self.textcolor = "black"
         self.posx = None
         self.posy = None
         self.dim = dim
@@ -84,7 +111,7 @@ class draughtpiece:
         if self.posx == None and self.posy == None:
             
             self.piece = self.canvas.create_oval(x-int(self.dim/2), y-int(self.dim/2), x+(self.dim/2), y+(self.dim/2), fill=self.color)
-            self.text = self.canvas.create_text(x, y, text=str(self.id), fill="white", font=("Times", 15))
+            self.text = self.canvas.create_text(x, y, text=str(self.id), fill=self.textcolor, font=("Times", 15))
         else:
             
             self.canvas.move(self.piece,x-self.posx, y-self.posy)
@@ -96,3 +123,10 @@ class draughtpiece:
 if __name__=="__main__":
 
     db = draughtsboard()
+    state = np.zeros([8, 8, 3])
+    state[0, 0, 1] = 2
+    state[0, 0, 2] = 1
+    state[7, 7, 1] = 1
+    state[7, 7, 2] = 2
+    db.settostate(state)
+    db.window.mainloop()
